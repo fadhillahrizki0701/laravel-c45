d155d453a61844daf11f3736d25f29bcb27a8abd
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-08-20 22:25:22 +0700

    feat: add CSRF token in blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>C4.5 - @yield('title')</title>
+
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>C4.5 - @yield('title')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         {{-- Datatables --}}
         <link rel="stylesheet" href="{{ asset('datatables/datatables.min.css') }}">
