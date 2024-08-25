9d0de958e996e2ccd43ef2e916f54c4837fde62c
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-08-25 22:11:47 +0700

    feat: set edit and delete feature in data dashboard pages

--- resources/views/pages/dataset1-index.blade.php
+++ resources/views/pages/dataset1-index.blade.php
@@ -46,6 +46,7 @@
                             <div class="mb-3">
                                 <label for="Usia" class="form-label">Usia (bulan)</label>
                                 <select class="form-select" id="Usia" name="Usia">
+                                    <option selected disabled>-- Silahkan Pilih --</option>
                                     @for ($i = 0; $i <= 70; $i++)
                                         <option value="{{ $i }}">{{ $i }}</option>
                                     @endfor
@@ -54,7 +55,7 @@
                             <div class="mb-3">
                                 <label for="berat_badan_per_usia" class="form-label">BB/U</label>
                                 <select class="form-select" id="berat_badan_per_usia" name="berat_badan_per_usia">
-                                    <option selected>Silahkan Pilih</option>
+                                    <option selected disabled>-- Silahkan Pilih --</option>
                                     <option value="{{ ucwords('normal') }}">Normal</option>
                                     <option value="{{ ucwords ('kurang') }}">Kurang</option>
                                     <option value="{{ ucwords ('sangat kurang') }}">Sangat Kurang</option>
@@ -63,7 +64,7 @@
                             <div class="mb-3">
                                 <label for="tinggi_badan_per_usia" class="form-label">TB/U</label>
                                 <select class="form-select" id="tinggi_badan_per_usia" name="tinggi_badan_per_usia">
-                                    <option selected>Silahkan Pilih</option>
+                                    <option selected disabled>-- Silahkan Pilih --</option>
                                     <option value="{{ ucwords('normal') }}">Normal</option>
                                     <option value="{{ ucwords('pendek') }}">Pendek</option>
                                     <option value="{{ ucwords('sangat pendek') }}">Sangat Pendek</option>
@@ -72,14 +73,14 @@
                             <div class="mb-3">
                                 <label for="berat_badan_per_tinggi_baadan" class="form-label">BB/TB</label>
                                 <select class="form-select" id="berat_badan_per_tinggi_badan" name="berat_badan_per_tinggi_badan">
-                                    <option selected>Silahkan Pilih</option>
+                                    <option selected disabled>-- Silahkan Pilih --</option>
                                     <option value="{{ ucwords('gizi baik') }}">Gizi Baik</option>
                                     <option value="{{ ucwords('gizi kurang') }}">Gizi Kurang</option>
                                 </select>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Save changes</button>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                <button type="submit" class="btn btn-primary">Simpan</button>
                             </div>
                         </form>
                     </div>
@@ -87,6 +88,15 @@
             </div>
         </div>
     </section>
+
+    <details class="my-3 p-2">
+        <summary class="fs-5">Keterangan</summary>
+        <ul>
+            <li><strong>BB/U</strong>, berat badan per usia</li>
+            <li><strong>TB/U</strong>, tinggi badan per usia</li>
+            <li><strong>BB/TB</strong>, berat badan per tinggi badan</li>
+        </ul>
+    </details>
 
     <section class="table-responsive">
         <table id="example" class="display" style="width:100%">
@@ -112,24 +122,111 @@
                         <td>{{ucwords($dt1->berat_badan_per_usia)  }}</td>
                         <td>{{ucwords($dt1->tinggi_badan_per_usia ) }}</td>
                         <td>{{ucwords( $dt1->berat_badan_per_tinggi_badan )}}</td>
-                        
                         @hasanyrole('admin|admin puskesmas')
                             <td>
                                 <section class="d-flex gap-2">
-                                    <a href="{{ route('dataset1.edit', $dt1->id) }}" class="btn btn-sm btn-warning text-white">
+                                    <button type="button" class="btn btn-sm btn-warning text-white" data-bs-toggle="modal" data-bs-target="#edit_{{ $dt1->id }}">
                                         <i class="bi bi-pencil-square"></i>
-                                    </a>
-                                    <form action="{{ route('dataset1.destroy', $dt1->id) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-danger text-white">
-                                            <i class="bi bi-trash"></i>
-                                        </button>
-                                    </form>
+                                    </button>
+                                    <button type="button" class="btn btn-sm btn-danger text-white" data-bs-toggle="modal" data-bs-target="#delete_{{ $dt1->id }}">
+                                        <i class="bi bi-trash"></i>
+                                    </button>
                                 </section>
                             </td>
                         @endhasanyrole
                     </tr>
+
+                    {{-- Edit --}}
+                    <div class="modal fade" id="edit_{{ $dt1->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="edit_{{ $dt1->id }}_label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="edit_{{ $dt1->id }}_label">Edit Data</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="{{ route('dataset1.update', $dt1->id) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="mb-3">
+                                            <label for="Nama" class="form-label">Nama</label>
+                                            <input type="text" class="form-control" id="Nama" name="Nama" placeholder="Masukkan Nama" value="{{ $dt1->Nama }}">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="Usia" class="form-label">Usia (bulan)</label>
+                                            <select class="form-select" id="Usia" name="Usia">
+                                                @for ($i = 0; $i <= 70; $i++)
+                                                    <option value="{{ $i }}" {{ $dt1->Usia == $i ? 'selected' : '' }}>{{ $i }}</option>
+                                                @endfor
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="berat_badan_per_usia" class="form-label">BB/U</label>
+                                            <select class="form-select" id="berat_badan_per_usia" name="berat_badan_per_usia">
+                                                <option value="{{ ucwords('normal') }}"  @selected(strtolower($dt1->berat_badan_per_usia) == strtolower('normal'))>Normal</option>
+                                                <option value="{{ ucwords('kurang') }}"  @selected(strtolower($dt1->berat_badan_per_usia) == strtolower('kurang'))>Kurang</option>
+                                                <option value="{{ ucwords('sangat kurang') }}"  @selected(strtolower($dt1->berat_badan_per_usia) == strtolower('sangat kurang'))>Sangat Kurang</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="tinggi_badan_per_usia" class="form-label">TB/U</label>
+                                            <select class="form-select" id="tinggi_badan_per_usia" name="tinggi_badan_per_usia">
+                                                <option value="{{ ucwords('normal') }}" @selected(strtolower($dt1->tinggi_badan_per_usia) == strtolower('normal'))>Normal</option>
+                                                <option value="{{ ucwords('pendek') }}" @selected(strtolower($dt1->tinggi_badan_per_usia) == strtolower('pendek'))>Pendek</option>
+                                                <option value="{{ ucwords('sangat pendek') }}" @selected(strtolower($dt1->tinggi_badan_per_usia) == strtolower('sangat pendek'))>Sangat Pendek</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="berat_badan_per_tinggi_badan" class="form-label">BB/TB</label>
+                                            <select class="form-select" id="berat_badan_per_tinggi_badan" name="berat_badan_per_tinggi_badan">
+                                                <option value="{{ ucwords('gizi baik') }}" @selected(strtolower($dt1->berat_badan_per_tinggi_badan) == strtolower('gizi baik'))>Gizi Baik</option>
+                                                <option value="{{ ucwords('gizi kurang') }}" @selected(strtolower($dt1->berat_badan_per_tinggi_badan) == strtolower('gizi kurang'))>Gizi Kurang</option>
+                                            </select>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                            <button type="submit" class="btn btn-warning">Simpan</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- End Edit --}}
+
+                    {{-- Delete --}}
+                    <div class="modal fade" id="delete_{{ $dt1->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="delete_{{ $dt1->id }}_label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="delete_{{ $dt1->id }}_label">Hapus Data</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="{{ route('dataset1.destroy', $dt1->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <p>Yakin ingin menghapus data ini?</p>
+                                        <details class="mt-2 mb-3 p-2 bg-light rounded border">
+                                            <summary>Rincian</summary>
+                                            <ul>
+                                                <li>Nama: <i>{{ $dt1->Nama }}</i></li>
+                                                <li>Usia: <i>{{ $dt1->Usia }}</i></li>
+                                                <li>BB/U: <i>{{ ucwords($dt1->berat_badan_per_usia) }}</i></li>
+                                                <li>TB/U: <i>{{ ucwords($dt1->tinggi_badan_per_usia) }}</i></li>
+                                                <li>BB/TB: <i>{{ ucwords($dt1->berat_badan_per_tinggi_badan) }}</i></li>
+                                            </ul>
+                                        </details>
+                                        <section class="d-flex gap-3">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                            <button type="submit" class="btn btn-danger">Ya, Hapus</button>
+                                        </section>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- End Delete --}}
                 @endforeach
             </tbody>
         </table>
--- resources/views/pages/dataset2-index.blade.php
+++ resources/views/pages/dataset2-index.blade.php
@@ -41,7 +41,8 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="Usia" class="form-label">Usia (bulan)</label>
-                                <select class="form-select" id="Usia" name="Usia" placeholder="Silahkan Pilih">
+                                <select class="form-select" id="Usia" name="Usia">
+                                    <option selected disabled>-- Silahkan Pilih --</option>
                                     @for ($i = 0; $i <= 70; $i++)
                                         <option value="{{ $i }}">{{ $i }}</option>
                                     @endfor
@@ -49,31 +50,32 @@
                             </div>
                             <div class="mb-3">
                                 <label for="berat_badan_per_tinggi_badan" class="form-label">BB/TB</label>
-                                <select class="form-select" id="berat_badan_per_tinggi_badan" name="berat_badan_per_tinggi_badan" placeholder="Silahkan Pilih">
-                                    <option selected>Silahkan Pilih</option>
-                                    <option value="gizi baik">Gizi Baik</option>
-                                    <option value="gizi kurang">Gizi Kurang</option>
+                                <select class="form-select" id="berat_badan_per_tinggi_badan" name="berat_badan_per_tinggi_badan">
+                                    <option selected disabled>-- Silahkan Pilih --</option>
+                                    <option value="{{ ucwords('gizi baik') }}">Gizi Baik</option>
+                                    <option value="{{ ucwords('gizi kurang') }}">Gizi Kurang</option>
                                 </select>
                             </div>
                             <div class="mb-3">
                                 <label for="Menu" class="form-label">Menu Makanan</label>
-                                <select class="form-select" id="Menu" name="Menu" placeholder="Silahkan Pilih">
+                                <select class="form-select" id="Menu" name="Menu">
+                                    <option selected disabled>-- Silahkan Pilih --</option>
                                     @for ($i = 0; $i <= 18; $i++)
-                                        <option value="{{ $i }}">{{ $i }}</option>
+                                        <option value="{{ "M$i" }}">{{ "M$i" }}</option>
                                     @endfor
                                 </select>
                             </div>
                             <div class="mb-3">
                                 <label for="Keterangan" class="form-label">Keterangan</label>
                                 <select class="form-select" id="Keterangan" name="Keterangan" placeholder="Silahkan Pilih">
-                                    <option selected>Silahkan Pilih</option>
-                                    <option value="baik">Baik</option>
-                                    <option value="tidak baik">Tidak Baik</option>
+                                    <option selected disabled>-- Silahkan Pilih --</option>
+                                    <option value="{{ ucwords('baik') }}">Baik</option>
+                                    <option value="{{ ucwords('tidak baik') }}">Tidak Baik</option>
                                 </select>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Save changes</button>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                <button type="submit" class="btn btn-primary">Simpan</button>
                             </div>
                         </form>
                     </div>
@@ -82,8 +84,14 @@
         </div>
     </section>
 
+    <details class="my-3 p-2">
+        <summary class="fs-5">Keterangan</summary>
+        <ul>
+            <li><strong>BB/TB</strong>, berat badan per tinggi badan</li>
+        </ul>
+    </details>
+
     <section class="table-responsive">
-    
         <table id="example" class="display" style="width:100%">
             <thead>
                 <tr>
@@ -105,24 +113,105 @@
                         <td>{{ ucwords($dt2->berat_badan_per_tinggi_badan) }}</td>
                         <td>{{ $dt2->Menu }}</td>
                         <td>{{ ucwords($dt2->Keterangan) }}</td>
-                        
                         @hasanyrole('admin|admin puskesmas')
                             <td>
                                 <section class="d-flex gap-2">
-                                    <a href="{{ route('dataset2.edit', $dt2->id) }}" class="btn btn-sm btn-warning text-white">
+                                    <button type="button" class="btn btn-sm btn-warning text-white" data-bs-toggle="modal" data-bs-target="#edit_{{ $dt2->id }}">
                                         <i class="bi bi-pencil-square"></i>
-                                    </a>
-                                    <form action="{{ route('dataset2.destroy', $dt2->id) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-danger text-white">
-                                            <i class="bi bi-trash"></i>
-                                        </button>
-                                    </form>
+                                    </button>
+                                    <button type="button" class="btn btn-sm btn-danger text-white" data-bs-toggle="modal" data-bs-target="#delete_{{ $dt2->id }}">
+                                        <i class="bi bi-trash"></i>
+                                    </button>
                                 </section>
                             </td>
                         @endhasanyrole
                     </tr>
+
+                    {{-- Edit --}}
+                    <div class="modal fade" id="edit_{{ $dt2->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="edit_{{ $dt2->id }}_label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="edit_{{ $dt2->id }}_label">Edit Data</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="{{ route('dataset2.update', $dt2->id) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="mb-3">
+                                            <label for="Usia" class="form-label">Usia (bulan)</label>
+                                            <select class="form-select" id="Usia" name="Usia">
+                                                @for ($i = 0; $i <= 70; $i++)
+                                                    <option value="{{ $i }}" {{ $dt2->Usia == $i ? 'selected' : '' }}>{{ $i }}</option>
+                                                @endfor
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="berat_badan_per_tinggi_badan" class="form-label">BB/TB</label>
+                                            <select class="form-select" id="berat_badan_per_tinggi_badan" name="berat_badan_per_tinggi_badan">
+                                                <option value="{{ ucwords('gizi baik') }}" @selected(strtolower($dt2->berat_badan_per_tinggi_badan) == strtolower('gizi baik'))>Gizi Baik</option>
+                                                <option value="{{ ucwords('gizi kurang') }}" @selected(strtolower($dt2->berat_badan_per_tinggi_badan) == strtolower('gizi kurang'))>Gizi Kurang</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="Menu" class="form-label">Menu Makanan</label>
+                                            <select class="form-select" id="Menu" name="Menu" placeholder="Silahkan Pilih">
+                                                @for ($i = 0; $i <= 18; $i++)
+                                                    <option value="M{{ $i }}" @selected($dt2->Menu == "M$i")>{{ "M{$i}" }}</option>
+                                                @endfor
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="Keterangan" class="form-label">Keterangan</label>
+                                            <select class="form-select" id="Keterangan" name="Keterangan" placeholder="Silahkan Pilih">
+                                                <option value="{{ ucwords('baik') }}" @selected(strtolower($dt2->Keterangan) == strtolower('baik'))>Baik</option>
+                                                <option value="{{ ucwords('tidak baik') }}" @selected(strtolower($dt2->Keterangan) == strtolower('tidak baik'))>Tidak Baik</option>
+                                            </select>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                            <button type="submit" class="btn btn-warning">Simpan</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- End Edit --}}
+
+                    {{-- Delete --}}
+                    <div class="modal fade" id="delete_{{ $dt2->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="delete_{{ $dt2->id }}_label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="delete_{{ $dt2->id }}_label">Hapus Data</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="{{ route('dataset2.destroy', $dt2->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <p>Yakin ingin menghapus data ini?</p>
+                                        <details class="mt-2 mb-3 p-2 bg-light rounded border">
+                                            <summary>Rincian</summary>
+                                            <ul>
+                                                <li>Usia: <i>{{ $dt2->Usia }}</i></li>
+                                                <li>BB/TB: <i>{{ ucwords($dt2->berat_badan_per_tinggi_badan) }}</i></li>
+                                                <li>Menu: <i>{{ ucwords($dt2->Menu) }}</i></li>
+                                                <li>Keterangan: <i>{{ ucwords($dt2->Keterangan) }}</i></li>
+                                            </ul>
+                                        </details>
+                                        <section class="d-flex gap-3">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                            <button type="submit" class="btn btn-danger">Ya, Hapus</button>
+                                        </section>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- End Delete --}}
                 @endforeach
             </tbody>
         </table>
