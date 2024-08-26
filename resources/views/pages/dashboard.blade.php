b5beea6816eabfaa67cf4efdd3aac7671f72e7df
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-08-26 22:04:22 +0700

    feat: optimize dashboard card

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -14,12 +14,17 @@
             <div class="card bg-white text-dark">
                 <div class="card-body px-4 py-4-5">
                     <div class="row">
-                        <div class="col-sm-12 col-md-4 mb-2">
-                            <div class="d-flex justify-content-center align-items-center p-2 rounded text-light" style="background-color: #b283fd;">
+                        <div class="col-sm-12 col-md-4">
+                            <div class="d-flex justify-content-center align-items-center p-2 rounded text-light" style="background-color: #b283fd; min-width: 50px;">
                                 <i class="bi-bar-chart-line-fill fs-1"></i>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-8">
+                        <div class="col-sm-12 col-md-8 mt-3 d-md-none text-center">
+                            <h5 class="card-title font-semibold" style="color: #995bfd;">Data 1</h5>
+                            <hr class="my-2">
+                            <h6 class="fs-5 text-muted">{{ $totaldataset1 }}</h6>
+                        </div>
+                        <div class="col-sm-12 col-md-8 d-none d-md-block">
                             <h5 class="card-title font-semibold" style="color: #995bfd;">Data 1</h5>
                             <hr class="my-2">
                             <h6 class="fs-5 text-muted">{{ $totaldataset1 }}</h6>
@@ -32,12 +37,17 @@
             <div class="card bg-white text-dark">
                 <div class="card-body px-4 py-4-5">
                     <div class="row">
-                        <div class="col-sm-12 col-md-4 mb-2">
-                            <div class="d-flex justify-content-center align-items-center p-2 rounded text-light" style="background-color: #47b8ff;">
+                        <div class="col-sm-12 col-md-4">
+                            <div class="d-flex justify-content-center align-items-center p-2 rounded text-light" style="background-color: #47b8ff; min-width: 50px;">
                                 <i class="bi-bar-chart-line-fill fs-1"></i>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-8">
+                        <div class="col-sm-12 col-md-8 mt-3 d-md-none text-center">
+                            <h5 class="card-title font-semibold" style="color: #26acff;">Data 2</h5>
+                            <hr class="my-2">
+                            <h6 class="fs-5 text-muted">{{ $totaldataset2 }}</h6>
+                        </div>
+                        <div class="col-sm-12 col-md-8 d-none d-md-block">
                             <h5 class="card-title font-semibold" style="color: #26acff;">Data 2</h5>
                             <hr class="my-2">
                             <h6 class="fs-5 text-muted">{{ $totaldataset2 }}</h6>
@@ -50,12 +60,17 @@
             <div class="card bg-white text-dark">
                 <div class="card-body px-4 py-4-5">
                     <div class="row">
-                        <div class="col-sm-12 col-md-4 mb-2">
-                            <div class="d-flex justify-content-center align-items-center p-2 rounded text-light" style="background-color: #ff2088;">
+                        <div class="col-sm-12 col-md-4">
+                            <div class="d-flex justify-content-center align-items-center p-2 rounded text-light" style="background-color: #ff2088; min-width: 50px;">
                                 <i class="bi-people fs-1"></i>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-8">
+                        <div class="col-sm-12 col-md-8 mt-3 d-md-none text-center">
+                            <h5 class="card-title font-semibold" style="color: #ff2088;">Pengguna</h5>
+                            <hr class="my-2">
+                            <h6 class="fs-5 text-muted">{{ $totalusers }}</h6>
+                        </div>
+                        <div class="col-sm-12 col-md-8 d-none d-md-block">
                             <h5 class="card-title font-semibold" style="color: #ff2088;">Pengguna</h5>
                             <hr class="my-2">
                             <h6 class="fs-5 text-muted">{{ $totalusers }}</h6>
