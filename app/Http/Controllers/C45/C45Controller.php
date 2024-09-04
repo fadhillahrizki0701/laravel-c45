1f29dd7907c5cb84b0d80d5210665b8962406c7a
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-09-04 09:24:38 +0700

    draft: progress decision tree #1

--- app/Http/Controllers/C45/C45Controller.php
+++ app/Http/Controllers/C45/C45Controller.php
@@ -3,12 +3,51 @@
 namespace App\Http\Controllers\C45;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Dataset1;
-use App\Models\Dataset2;
 
 class C45Controller extends Controller
 {
+    // Function to calculate the mean of a given attribute
+    public function calculateMean($data, $attribute)
+    {
+        $values = array_column($data, $attribute);
+        return array_sum($values) / count($values);
+    }
+
+    // Function to calculate the median of a given attribute
+    public function calculateMedian($data, $attribute)
+    {
+        $values = array_column($data, $attribute);
+        sort($values);
+        $count = count($values);
+        $middle = floor(($count - 1) / 2);
+        if ($count % 2) {
+            return $values[$middle];
+        } else {
+            return ($values[$middle] + $values[$middle + 1]) / 2.0;
+        }
+    }
+
+    // Function to group 'Usia' values based on mean and median
+    public function groupByCustomAttributes($data, $mean, $median)
+    {
+        foreach ($data as &$row) {
+            if ($row['usia'] <= $mean) {
+                $row['usia_group'] = 'below_mean';
+            } else {
+                $row['usia_group'] = 'above_mean';
+            }
+
+            if ($row['usia'] <= $median) {
+                $row['usia_group_median'] = 'below_median';
+            } else {
+                $row['usia_group_median'] = 'above_median';
+            }
+        }
+        return $data;
+    }
+
+    // Function to calculate entropy for a given set of data
     public function calculateEntropy($data, $labelAttribute)
     {
         $total = count($data);
@@ -24,6 +63,7 @@ class C45Controller extends Controller
         return $entropy;
     }
 
+    // Function to calculate Split Info for a given attribute
     public function calculateSplitInfo($data, $attribute)
     {
         $total = count($data);
@@ -39,6 +79,7 @@ class C45Controller extends Controller
         return $splitInfo;
     }
 
+    // Function to calculate Gain for a given attribute
     public function calculateGain($data, $attribute, $labelAttribute)
     {
         $totalEntropy = $this->calculateEntropy($data, $labelAttribute);
@@ -56,6 +97,7 @@ class C45Controller extends Controller
         return $totalEntropy - $subsetEntropy;
     }
 
+    // Function to calculate Gain Ratio for a given attribute
     public function calculateGainRatio($data, $attribute, $labelAttribute)
     {
         $gain = $this->calculateGain($data, $attribute, $labelAttribute);
@@ -68,16 +110,25 @@ class C45Controller extends Controller
         return $gain / $splitInfo;
     }
 
+    // Main function to process the node based on given data and attributes
     public function processNode($data, $attributes, $labelAttribute)
     {
         $output = [];
         $bestAttribute = null;
         $bestGainRatio = -INF;
 
+        // Calculate mean and median for Usia
+        $mean = $this->calculateMean($data, 'usia');
+        $median = $this->calculateMedian($data, 'usia');
+
+        // Group Usia based on mean and median
+        $groupedData = $this->groupByCustomAttributes($data, $mean, $median);
+
+        // Iterate through attributes to find the best one based on Gain Ratio
         foreach ($attributes as $attribute) {
-            $gain = $this->calculateGain($data, $attribute, $labelAttribute);
-            $splitInfo = $this->calculateSplitInfo($data, $attribute);
-            $gainRatio = $this->calculateGainRatio($data, $attribute, $labelAttribute);
+            $gain = $this->calculateGain($groupedData, $attribute, $labelAttribute);
+            $splitInfo = $this->calculateSplitInfo($groupedData, $attribute);
+            $gainRatio = $this->calculateGainRatio($groupedData, $attribute, $labelAttribute);
 
             // Prepare the structure of the output based on the Excel calculation format
             $attributeData = [
@@ -86,9 +137,9 @@ class C45Controller extends Controller
                 'GAIN_RATIO' => $gainRatio,
             ];
 
-            $values = array_unique(array_column($data, $attribute));
+            $values = array_unique(array_column($groupedData, $attribute));
             foreach ($values as $value) {
-                $subset = array_filter($data, function ($row) use ($attribute, $value) {
+                $subset = array_filter($groupedData, function ($row) use ($attribute, $value) {
                     return $row[$attribute] == $value;
                 });
                 $labelCounts = array_count_values(array_column($subset, $labelAttribute));
@@ -98,7 +149,7 @@ class C45Controller extends Controller
                 $attributeData[$value] = [
                     'label_counts' => $labelCounts,
                     'entropy' => $entropy,
-                    'probability' => count($subset) / count($data),
+                    'probability' => count($subset) / count($groupedData),
                 ];
             }
 
@@ -117,20 +168,12 @@ class C45Controller extends Controller
         return $output;
     }
 
+    // Function to fetch and process the dataset for tree construction
     public function fetchTreeDataset1()
     {
         $data = Dataset1::all()->toArray();
-        $attributes = ["usia", "berat_badan_per_usia", "tinggi_badan_per_usia"];
+        $attributes = ["usia_group", "usia_group_median", "berat_badan_per_usia", "tinggi_badan_per_usia"];
         $labelAttribute = "berat_badan_per_tinggi_badan"; // Specify the label attribute
-        $rootNodeData = $this->processNode($data, $attributes, $labelAttribute);
-        return response()->json($rootNodeData);
-    }
-
-    public function fetchTreeDataset2()
-    {
-        $data = Dataset2::all()->toArray();
-        $attributes = ["usia", "menu"];
-        $labelAttribute = "keterangan"; // Specify the label attribute
         $rootNodeData = $this->processNode($data, $attributes, $labelAttribute);
         return response()->json($rootNodeData);
     }
