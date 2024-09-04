538269b20abdcf25995c00240e5722431497b33f
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-09-04 11:43:00 +0700

    draft: progress decision tree #2

--- app/Http/Controllers/C45/DecisionTreeNodeController.php
+++ app/Http/Controllers/C45/DecisionTreeNodeController.php
@@ -7,15 +7,15 @@ use Illuminate\Http\Request;
 
 class DecisionTreeNodeController extends Controller
 {
-	public $attribute;
-	public $value;
-	public $children = [];
-	public $isLeaf = false;
-	public $label;
+    public $attribute;
+    public $value;
+    public $children = [];
+    public $isLeaf = false;
+    public $label;
 
-	public function __construct($attribute = null, $value = null)
-	{
-		$this->attribute = $attribute;
-		$this->value = $value;
-	}
+    public function __construct($attribute = null, $value = null)
+    {
+        $this->attribute = $attribute;
+        $this->value = $value;
+    }
 }
