845257dd6751bff4eb3f1144f5b5f642021b38cc
fadhillahrizki0701 <fadhillahrizki0701@gmail.com>
2024-10-15 21:51:02 +0700

    refactor: delete unused button menu

--- resources/views/pages/datatrain2-index.blade.php
+++ resources/views/pages/datatrain2-index.blade.php
@@ -5,38 +5,6 @@
 @section('content')
 <section class="container p-4">
     <h2 class="pb-4" style="color:#435EBE">Data Training 2</h2>
-
-    @include('pages.partials.session-notification')
-
-    <form action="{{ route('datatrain2.store') }}" method="POST" enctype="multipart/form-data" class="">
-        @csrf
-        <label for="file">Impor data dari Excel</label>
-        <div class="input-group my-3">
-            <input type="file" name="file" id="file" class="form-control" accept=".csv,.xlsx">
-            <button type="submit" class="btn btn-success">Unggah Data</button>
-        </div>
-    </form>
-
-    @role('admin')
-        <form action="{{ route('datatrain2.clear') }}" method="POST" onsubmit="return confirm('Yakin ingin menghapus seluruh file beserta isinya?');">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Bersihkan Data</button>
-        </form>
-    @endrole
-
-    <details class="my-3 p-2">
-        <summary class="fs-5">Keterangan</summary>
-        <ul>
-            <li><strong>BB/TB</strong>, berat badan per tinggi badan</li>
-        </ul>
-    </details>
-
-<p class="mt-3"> Harap pastikan file CSV mengikuti format di bawah ini:</p>
-<pre class="mt-2">usia (bulan);BB_TB;menu;keterangan
-12;Gizi Baik;M15;Tidak Baik
-18;Gizi Baik;M9;Baik
-...</pre>
     
     <a href="{{ route('datatrain2-mining') }}" class="btn btn-success">Proses Mining</a>
 
