3e99031c49ffeaa9afd0a43be1136fb0f5c5c020
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2024-11-27 11:18:49 +0700

    refactor(ui): update rules and confusion matrix layouting

--- resources/views/pages/datatest1-index.blade.php
+++ resources/views/pages/datatest1-index.blade.php
@@ -83,86 +83,88 @@
             </table>
         </section>
     </section>
-    <section class="col mb-3">
-    <section class="bg-light my-3 p-3 border border-1">
-        @if(count($rules) > 0)
-            <details>
-                <summary><h5 class="d-inline">Rules</h5></summary>
-                <br/>
-                <ul style="list-style: none; margin-left: 0;" class="bg-white p-0 p-3 rounded text-secondary border border-1">
-                    @foreach ($rules as $rule)
-                        @php
-                            // Split the rule into lines
-                            $lines = explode("\n", trim($rule));
-                            $indentLevel = 0;  // Track indentation for nested IF statements
-                        @endphp
-                        <li>
-                            @foreach ($lines as $line)
-                                @if (strpos($line, 'IF') !== false)
-                                    @php
-                                        // Increase the indentation for nested IFs
-                                        $indentLevel++;
-                                        // Add indentation spaces based on the nesting level
-                                        $indentation = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $indentLevel - 1);
-                                    @endphp
-                                    {!! $indentation . str_replace('IF', '├', $line) !!}<br>
-                                @elseif (strpos($line, 'THEN') !== false)
-                                    @php
-                                        // Reset the indentation level for THEN
-                                        $indentation = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $indentLevel);
-                                    @endphp
-                                    {!! $indentation . str_replace('THEN', '┗╸', $line) !!}<br>
-                                    @php $indentLevel = 0; @endphp  {{-- Reset for next rule set --}}
-                                @endif
+
+    <section class="row gap-y-3 my-4">
+        <section class="col mb-3">
+            <section class="bg-light p-3 border border-1">
+                <details>
+                    <summary class=""><h5 class="d-inline">Rules</h5></summary>
+                    <br/>
+                    @if(count($rules) > 0)
+                        <ul style="list-style: none; margin-left: 0;" class="bg-white p-0 p-3 rounded text-secondary border border-1  overflow-x-auto">
+                            @foreach ($rules as $rule)
+                                @php
+                                    // Split the rule into lines
+                                    $lines = explode("\n", trim($rule));
+                                    $indentLevel = 0;  // Track indentation for nested IF statements
+                                @endphp
+                                <li>
+                                    @foreach ($lines as $line)
+                                        @if (strpos($line, 'IF') !== false)
+                                            @php
+                                                // Increase the indentation for nested IFs
+                                                $indentLevel++;
+                                                // Add indentation spaces based on the nesting level
+                                                $indentation = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $indentLevel - 1);
+                                            @endphp
+                                            {!! $indentation . str_replace('IF', '├', $line) !!}<br>
+                                        @elseif (strpos($line, 'THEN') !== false)
+                                            @php
+                                                // Reset the indentation level for THEN
+                                                $indentation = str_repeat('&nbsp;&nbsp;&nbsp;&nbsp;', $indentLevel);
+                                            @endphp
+                                            {!! $indentation . str_replace('THEN', '┗╸', $line) !!}<br>
+                                            @php $indentLevel = 0; @endphp  {{-- Reset for next rule set --}}
+                                        @endif
+                                    @endforeach
+                                </li>
                             @endforeach
-                        </li>
-                    @endforeach
-                </ul>
-                <hr>
-            </details>
-        @endif
-    </section>
-    <section class="col mb-3">
-        <div class="card" style="width: 100%;">
-            <div class="card-body bg-light">
-                <h5 class="card-title">Confusion Matrix</h5>
-                <table class="table text-nowrap my-4">
-                    <thead>
-                        <tr>
-                            <th rowspan="2">Aktual</th>
-                            <th colspan="2" class="text-center">Prediksi</th>
-                        </tr>
-                        <tr>
-                            @foreach ($metrices['labels'] as $label)
-                                <th>{{ $label }}</th>
-                            @endforeach
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>{{ $metrices['labels'][0] ?? '' }}</th>
-                            <td>{{ $metrices['confusion_matrix']['TP'] ?? '' }}</td>
-                            <td>{{ $metrices['confusion_matrix']['TN'] ?? '' }}</td>
-                        </tr>
-                        <tr>
-                            <th>{{ $metrices['labels'][1] ?? '' }}</th>
-                            <td>{{ $metrices['confusion_matrix']['FP'] ?? '' }}</td>
-                            <td>{{ $metrices['confusion_matrix']['FN'] ?? '' }}</td>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <th>Total</th>
-                            <td>{{ $metrices['confusion_matrix']['TP'] ?? 0 + $metrices['confusion_matrix']['FP'] ?? 0 }}</td>
-                            <td>{{ $metrices['confusion_matrix']['TN'] ?? 0 + $metrices['confusion_matrix']['FN'] ?? 0 }}</td>
-                        </tr>
-                    </tfoot>
-                </table>
-                <button class="btn btn-info" type="button"><i class="bi bi-info-square-fill"></i> {{ $metrices['accuracy'] }}%</button>
+                        </ul>
+                    @endif
+                </details>
+            </section>
+        </section>
+        <section class="col mb-3">
+            <div class="card" style="width: 100%;">
+                <div class="card-body bg-light">
+                    <h5 class="card-title">Confusion Matrix</h5>
+                    <table class="table text-nowrap my-4">
+                        <thead>
+                            <tr>
+                                <th rowspan="2">Aktual</th>
+                                <th colspan="2" class="text-center">Prediksi</th>
+                            </tr>
+                            <tr>
+                                @foreach ($metrices['labels'] as $label)
+                                    <th>{{ $label }}</th>
+                                @endforeach
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>{{ $metrices['labels'][0] ?? '' }}</th>
+                                <td>{{ $metrices['confusion_matrix']['TP'] ?? '' }}</td>
+                                <td>{{ $metrices['confusion_matrix']['TN'] ?? '' }}</td>
+                            </tr>
+                            <tr>
+                                <th>{{ $metrices['labels'][1] ?? '' }}</th>
+                                <td>{{ $metrices['confusion_matrix']['FP'] ?? '' }}</td>
+                                <td>{{ $metrices['confusion_matrix']['FN'] ?? '' }}</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Total</th>
+                                <td>{{ $metrices['confusion_matrix']['TP'] ?? 0 + $metrices['confusion_matrix']['FP'] ?? 0 }}</td>
+                                <td>{{ $metrices['confusion_matrix']['TN'] ?? 0 + $metrices['confusion_matrix']['FN'] ?? 0 }}</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                    <button class="btn btn-info" type="button"><i class="bi bi-info-square-fill"></i> {{ $metrices['accuracy'] }}%</button>
+                </div>
             </div>
-        </div>
+        </section>
     </section>
-</section>
 
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
